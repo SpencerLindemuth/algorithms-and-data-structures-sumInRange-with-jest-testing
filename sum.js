ad89dca63c284b75a8171267c4e2fae9da2eb211
SpencerLindemuth <47793873+SpencerLindemuth@users.noreply.github.com>
2019-11-05 13:37:45 -0800

    Add tests to type check inputs

--- sum.js
+++ sum.js
@@ -1,11 +1,15 @@
 function sumInRange(array, lowerLimit, upperLimit) {
-    let sum = 0;
-    while(upperLimit >= lowerLimit){
-        sum = sum + array[upperLimit];
-        upperLimit--;
+    if(array.length === 0 || !Number.isInteger(lowerLimit) || !Number.isInteger(upperLimit)){
+        return null;
+    }else{
+        let sum = 0;
+        while(upperLimit >= lowerLimit){
+            sum = sum + array[upperLimit];
+            upperLimit--;
+        }
+        return sum;
     }
-    return sum;
-  }
+}
 
 
   module.exports = sumInRange;
--- sum.test.js
+++ sum.test.js
@@ -4,6 +4,18 @@ test('There is a function named sumInRange', () => {
   expect(sumInRange);
 })
 
+test('Function handles empty array', () => {
+  expect(sumInRange([], 1, 4)).toBe(null)
+})
+
+test('Function handles invalid type of lower limit', () => {
+  expect(sumInRange([1,2,3,4], 'a', 5)).toBe(null)
+})
+
+test('Function handles invalid type of upper limit', () => {
+  expect(sumInRange([1,2,3,4], 0, 'a')).toBe(null)
+})
+
 test('Finds the sum of numbers in a given range', () => {
   let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
   let lowerLimit = 0;
